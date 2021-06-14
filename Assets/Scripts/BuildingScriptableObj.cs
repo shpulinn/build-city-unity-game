c1d1a4c80ab42371efafd38d796f1902721be168
Nikita <shpulinn@mail.ru>
2021-06-15 01:06:30 +0200

    BuildingScriptableObject created

---
+++ Assets/Scripts/BuildingScriptableObj.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Building", menuName = "Buildings")]
+public class BuildingScriptableObj : ScriptableObject {
+
+    public int type;
+    public int moneyCost;
+    public int woodCost;
+    public int brickCost;
+    public float buildTime;
+
+
+}
