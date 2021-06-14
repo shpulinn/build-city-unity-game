a0ba5e4f3bc8b64775d9af1fc0510c0084a77a0c
Nikita <shpulinn@mail.ru>
2021-06-15 01:08:04 +0200

    Update Building.cs
    attempt to realize building process

--- Assets/Scripts/Building.cs
+++ Assets/Scripts/Building.cs
@@ -1,8 +1,17 @@
+using System.Collections;
 using UnityEngine;
 
 public class Building : MonoBehaviour {
 
+    public BuildingScriptableObj BuildingScriptableObj;
+
     public Vector2Int Size = Vector2Int.one;
+
+    private MeshRenderer MeshRenderer;
+
+    private void Awake() {
+        MeshRenderer = GetComponentInChildren<MeshRenderer>();
+    }
 
     private void OnDrawGizmos() {
         for (int x = 0; x < Size.x; x++) {
@@ -11,5 +20,19 @@ public class Building : MonoBehaviour {
                 Gizmos.DrawCube(transform.position + new Vector3(x, -.5f, y), new Vector3(1, .1f, 1));
             }
         }
+    }
+
+    public void StartBuilding() {
+        float time = 0;
+        MeshRenderer.material.color = new Color(100, 0, 0);
+        for (int i = 0; i < BuildingScriptableObj.buildTime;) {
+            IEnumerator Wait() {
+                yield return new WaitForSeconds(1f);
+                i++;
+            }
+            StartCoroutine(Wait());
+            Debug.Log(i);
+        }
+        MeshRenderer.material.color = Color.white;
     }
 }
