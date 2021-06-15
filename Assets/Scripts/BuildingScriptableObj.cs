52a228d073f42677328e1ab5f3c91b323c08eb29
Nikita <shpulinn@mail.ru>
2021-06-15 19:58:42 +0200

    Update BuildingScriptableObj.cs
    type field changed from int to string

--- Assets/Scripts/BuildingScriptableObj.cs
+++ Assets/Scripts/BuildingScriptableObj.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "Building", menuName = "Buildings")]
 public class BuildingScriptableObj : ScriptableObject {
 
-    public int type;
+    public string type;
     public int moneyCost;
     public int woodCost;
     public int brickCost;
